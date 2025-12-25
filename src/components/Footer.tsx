7ff8ab4a9521685de38abbb08fe5fcf244e7b25f
Lim Haean <detols921@naver.com>
2025-12-26 03:20:53 +0900

    title fix

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,11 @@ export default function Footer() {
   return (
     <footer className="page__footer">
       <p className="page__footer-copy">
+<<<<<<< HEAD
         &copy; 2025, JBNU Web Service Design Final Project
+=======
+        &copy; 2025, JBNU Web Service Design Blog Project
+>>>>>>> 2cab23c (title fix)
       </p>
     </footer>
   );
